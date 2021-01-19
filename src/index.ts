ab25d72924bf1b47e8123d1e1c128fa31a38509f
Shinji Kawakami <skphysi929@gmail.com>
2021-01-19 13:03:31 +0900

    add the add custom clame function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions'
 import * as firebase from 'firebase-admin'
+import admin = require('firebase-admin');
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
@@ -19,4 +20,34 @@ export const checkInviteKey = functions.https.onCall( async ( inviteKey : string
     
     return (accessToken)
 
+});
+
+export const addCustomRole = functions.https.onCall( async ( {enterpriseId} : {
+    enterpriseId : string
+}, context: functions.https.CallableContext) => {
+    const { auth } = context
+    console.log('auth',auth)
+    if (!auth) {
+        return
+    }
+    const { uid } = auth
+    
+    return admin.auth().setCustomUserClaims(uid, {admin:false, enterpriseId:enterpriseId})
+    .then(()=>{
+        return {
+            message: `Success! ${uid} has been made an admin`
+        }
+    }).catch(err=>{
+        return err
+    })
+    // get user add customa claim 
+    // return admin.auth().getUserByEmail(email).then().then( user => {
+    //     return admin.auth().setCustomUserClaims(user.uid, {admin:false, enterpriseId:enterpriseId})
+    // }).then(()=>{
+    //     return {
+    //         message: `Success! ${email} has been made an admin`
+    //     }
+    // }).catch(err=>{
+    //     return err
+    // })
 });
